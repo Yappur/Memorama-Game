7a006b72553671de0574f735f510f4ab06c671de
unknown <mateox463@gmail.com>
2025-03-06 12:24:12 -0300

    se implementa footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import Board from "./components/Board";
 import BubbleBackground from "./components/BubbleBackground";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <>
+    <div>
       <div>
         <BubbleBackground />
         <Board />
       </div>
-    </>
+      <Footer />
+    </div>
   );
 }
 
